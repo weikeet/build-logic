d00db6646029878fb23cb10761d10466192615aa
Weiwei <1570682285@qq.com>
2022-04-13 16:57:10 +0800

    upgrade navigation 2.4.2

--- src/main/java/Dependencies.kt
+++ src/main/java/Dependencies.kt
@@ -177,7 +177,7 @@ object ArchitectureLibs {
   const val PagingRxJava2Ktx = "androidx.paging:paging-rxjava2-ktx:${PagingLibVer}"
 
   // https://mvnrepository.com/artifact/androidx.navigation
-  private const val NavigationLibVer = "2.4.1"
+  private const val NavigationLibVer = "2.4.2"
   const val NavigationFragment = "androidx.navigation:navigation-fragment:${NavigationLibVer}"
   const val NavigationFragmentKtx = "androidx.navigation:navigation-fragment-ktx:${NavigationLibVer}"
   const val NavigationUI = "androidx.navigation:navigation-ui:${NavigationLibVer}"
